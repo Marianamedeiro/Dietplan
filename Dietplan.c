dd86a0dd57e16dbb36546ea662f88e894a9fbd5c
Marianamedeiro <96838355+Marianamedeiro@users.noreply.github.com>
2021-12-30 11:37:46 -0300

    Add files via upload

---
+++ Dietplan.c
@@ -0,0 +1,99 @@
+#include <stdio.h>
+
+/////
+// Assinatura das funções
+
+void telaSobre(void);
+void telaPrincipal(void);
+void telaEquipe(void);
+
+/////
+// Programa principal
+
+int main(void) {
+    telaSobre();
+    telaPrincipal();
+    telaEquipe();
+    return 0;
+}
+
+// ///
+// Funções
+
+void telaSobre(void) {
+    printf("\n");
+    printf("////////////////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                                  ///\n");
+    printf("///                Universidade Federal do Rio Grande do Norte                       ///\n");
+    printf("///                Centro de Ensino Superior do Seridó                               ///\n");
+    printf("///                Departamento de Computação e Tecnologia                           ///\n");
+    printf("///                Disciplina DCT1106 - Programação                                  ///\n");
+    printf("///                Projeto Sistema de Planejamento de Dieta                          ///\n");
+    printf("///                Desenvolvido por mariana - Nov, 2021                              ///\n");
+    printf("///                                                                                  ///\n");
+    printf("////////////////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                                  ///\n");
+    printf("///             = = = = = Sistema de Planejamento de Dieta = = = = = =               ///\n");
+    printf("///                                                                                  ///\n");
+    printf("///         Programa  utilizado  na  disciplina  DCT1106 - Programação, para fins    ///\n");
+    printf("///         avaliativo no Semestre 2021.2. O programa contém os principais módulos   ///\n");
+    printf("///         e funcionalidades que serão exigidos ao longo da disciplina.             ///\n");
+    printf("///                                                                                  ///\n");
+    printf("////////////////////////////////////////////////////////////////////////////////////////\n");
+    printf("\n");
+}
+
+
+
+void telaPrincipal(void) {
+    printf("\n");
+    printf("////////////////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                                  ///\n");
+    printf("///                  Universidade Federal do Rio Grande do Norte                     ///\n");
+    printf("///                  Centro de Ensino Superior do Seridó                             ///\n");
+    printf("///                  Departamento de Computação e Tecnologia                         ///\n");
+    printf("///                  Disciplina DCT1106 - Programação                                ///\n");
+    printf("///                  Projeto Sistema de Planejamento de Dieta                        ///\n");
+    printf("///                  Desenvolvido por mariana - Nov, 2021                            ///\n");
+    printf("///                                                                                  ///\n");
+    printf("////////////////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                                  ///\n");
+    printf("///             = = = = = = Sistema de Planejamento de Dieta = = = = = =             ///\n");
+    printf("///                                                                                  ///\n");
+    printf("///             1. Módulo Cardápio                                                   ///\n");
+    printf("///             2. Módulo Paciente                                                   ///\n");
+    printf("///             3. Módulo Receita                                                    ///\n");
+    printf("///             4. Módulo Relatórios                                                 ///\n");
+    printf("///             5. Módulo Sobre                                                      ///\n");
+    printf("///             0. Sair                                                              ///\n");
+    printf("///                                                                                  ///\n");
+    printf("////////////////////////////////////////////////////////////////////////////////////////\n");
+    printf("\n");
+}
+
+
+
+void telaEquipe(void) {
+    printf("\n");
+    printf("////////////////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                                  ///\n");
+    printf("///                    Universidade Federal do Rio Grande do Norte                   ///\n");
+    printf("///                    Centro de Ensino Superior do Seridó                           ///\n");
+    printf("///                    Departamento de Computação e Tecnologia                       ///\n");
+    printf("///                    Disciplina DCT1106 - Programação                              ///\n");
+    printf("///                    Projeto Sistema de Planejamento de Dieta                      ///\n");
+    printf("///                    Desenvolvido por mariana - Nov, 2021                          ///\n");
+    printf("///                                                                                  ///\n");
+    printf("////////////////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                                  ///\n");
+    printf("///                 = = = = = Sistema de Planejamento de Dieta = = = = =             ///\n");
+    printf("///                                                                                  ///\n");
+    printf("///                 Este projeto foi desenvolvido por:                               ///\n");
+    printf("///                                                                                  ///\n");
+    printf("///                 Mariana Araújo de Medeiros                                       ///\n");
+    printf("///                 E-mail: mariana.medeiros.017@ufrn.edu.br                         ///\n");
+    printf("///                 Repositório: https://github.com/Got7mj/mariana                   ///\n");
+    printf("///                                                                                  ///\n");
+    printf("////////////////////////////////////////////////////////////////////////////////////////\n");
+    printf("/n");
+}
